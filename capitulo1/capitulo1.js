08737ffb53dcb1f07f213e1a38da07137f1a2e65
Skotpionx <skotpion15@gmail.com>
2022-04-05 11:03:30 +0200

    Document.write

--- capitulo1/capitulo1.js
+++ capitulo1/capitulo1.js
@@ -24,6 +24,27 @@ let n = null; // Vacia , le dices que no hay nada dentro > diferente de undefine
 
 // let nombre2 = prompt("Dime tu nombre");
 
-let nombre3 = prompt("Dime tu nombre");
+// let nombre3 = prompt("Dime tu nombre"); > Con prompt pregunttas el nombre al usuario y lo guardo en variable (o no)
 
-alert("Hola " + nombre);
+// alert("Hola " + nombre);
+
+numero1 = "52";
+
+numero2 = 8;
+
+frase = numero1.concat(numero2); // Hay que tener minimo un string para concatenar.
+
+// document.write(frase);
+
+//Otra forma de concatenar
+
+//Esta es una forma normal
+nombre = " Juanmi Fernandez";
+
+frase2 = "soy " + nombre + " y estoy caminando";
+
+// Esta es una forma buena se llama backticks 
+
+frase2 = `soy ${nombre} y estoy caminando`;
+
+document.write(frase2);
