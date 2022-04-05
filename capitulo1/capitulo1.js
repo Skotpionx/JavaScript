18d26cbbab9c77faa06ec38a4ba5289973ad5bff
Skotpionx <skotpion15@gmail.com>
2022-04-05 10:43:13 +0200

    Primeros 50 Min

--- capitulo1/capitulo1.js
+++ capitulo1/capitulo1.js
@@ -1,1 +1,29 @@
-alert("hola")
+const nombre = "Juanmi"
+
+// alert(nombre);
+
+let numero, numero2, numero3;
+
+numero = 2;
+numero2 = 34;
+numero3 = 35;
+
+// alert(numero);
+// alert(numero2);
+// alert(numero3);
+
+let numero4 = 23,
+    numero5 = 45;
+
+let numero6 = 45;
+let numero7 = 56;
+
+let n = null; // Vacia , le dices que no hay nada dentro > diferente de undefined.
+
+// alert(numero * nombre)  NaN > Not A Number > estás haciendo una operación con un número y algo que no es un número.
+
+// let nombre2 = prompt("Dime tu nombre");
+
+let nombre3 = prompt("Dime tu nombre");
+
+alert("Hola " + nombre);
